67973ae330ca30f2f855aed045c12f967279065a
MohitReezal <85101874+MohitReezal@users.noreply.github.com>
2024-05-30 22:25:47 +0545

    Delete html directory

--- html/java_projects/setbit.java
+++
@@ -1,9 +0,0 @@
-public class setbit {
-    public static void main(String args[]){
-        int n=5;
-        int pos= 1;
-        int bitmask= 1<<pos;
-        int newnum= bitmask | n;
-        System.out.println(newnum);
-    }
-}
